652eb661de282bae628c27cf7658923243a14ff8
Mori <mobileabi7@gmail.com>
2022-03-24 22:41:42 +0430

    example1

---
+++ 1.py
@@ -0,0 +1,27 @@
+#1
+'''برنامهای بنویسید که سه عدد از کاربر بگیرد و نسبت عدد اول را به دوعدد بعدی را چاپ نماید.
+پاسخ های مجاز :
+Bozogtar
+Koochektar
+Mosavi
+Vasat
+test git 
+'''
+
+if x1 > x2 and x1 > x3 :
+    print('-----------------')
+    print("Bozorgtar")
+elif x1 > x2 and x1 < x3 :
+    print('-----------------')
+    print('Vasat')
+elif x1 < x2 and x1 > x3 :
+    print('-----------------')
+    print('Vasat')
+elif x1 == x2 and x1 == x3 :
+    print('-----------------')
+    print("Mosavi")
+else :
+    print('-----------------')
+    print("Koochektar")
+
+
